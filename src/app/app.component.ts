cb4ca2f2a5a24326690ea676f8c014f281ccc522
MuchemiKelvin <kelvin.chuthi@zandaux.com>
2024-04-12 17:40:32 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+  HomeComponent,
+],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Homes';
+}
